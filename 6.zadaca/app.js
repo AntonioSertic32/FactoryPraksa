b8534ca8dbc67f1c86f3a33f239ab9a84c2a8217
Antonio <antonio.sertic@vsmti.hr>
2020-07-19 10:13:09 +0200

    Slajder pocetak

--- 6.zadaca/app.js
+++ 6.zadaca/app.js
@@ -208,3 +208,18 @@ setTimeout(() => {
   game.Finale();
 }, 2000);
 */
+
+$(".slider-for").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  fade: true,
+  asNavFor: ".slider-nav",
+});
+$(".slider-nav").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: ".slider-for",
+  centerMode: true,
+  focusOnSelect: true,
+});
